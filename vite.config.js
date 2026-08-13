224202c3b04b832411e82fa212f475d2751b9ffb
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-13 09:36:45 +0530

    installable-app update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Pulse Music Player",
+        short_name: "Pulse",
+        description: "A responsive React music player",
+        theme_color: "#0b1020",
+        background_color: "#0b1020",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
